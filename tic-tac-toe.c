86781a8df275c5908e6f40760a1a9f49af73f43f
Ronn Angelo Lee <rrlee@student.apc.edu.ph>
2019-12-04 16:40:46 +0000

    Gives condition to text displays to prevent unnecessary overwrite repetition

--- tic-tac-toe.c
+++ tic-tac-toe.c
@@ -37,46 +37,47 @@ int main() {
             if(windowState != 1 || screenY != currScreenY || screenX != currScreenX) {
                 windowState = 1;
                 clear();
+
+                currScreenY = screenY;
+                currScreenX = screenX;
+
+                mvprintw(screenY/2, (screenX - strlen("Expand window height and width."))/2, "Expand window height and width.");
             }
-            currScreenY = screenY;
-            currScreenX = screenX;
-
-            mvprintw(screenY/2, (screenX - strlen("Expand window height and width."))/2, "Expand window height and width.");
-
         } else if(screenX < strlen(movementInstruction)) {
             if(windowState != 2 || screenY != currScreenY || screenX != currScreenX) {
                 windowState = 2;
                 clear();
+
+                currScreenY = screenY;
+                currScreenX = screenX;
+
+                mvprintw(screenY/2, (screenX - strlen("Expand window width."))/2, "Expand window width.");
             }
-            currScreenY = screenY;
-            currScreenX = screenX;
-
-            mvprintw(screenY/2, (screenX - strlen("Expand window width."))/2, "Expand window width.");
-
         } else if(screenY < ribbonHeight + tableHeight) {
             if(windowState != 3 || screenY != currScreenY || screenX != currScreenX) {
                 windowState = 3;
                 clear();
+
+                currScreenY = screenY;
+                currScreenX = screenX;
+
+                mvprintw(screenY/2, (screenX - strlen("Expand window height."))/2, "Expand window height.");
             }
-            currScreenY = screenY;
-            currScreenX = screenX;
-
-            mvprintw(screenY/2, (screenX - strlen("Expand window height."))/2, "Expand window height.");
-
         } else {
             if(windowState != 4 || screenY != currScreenY || screenX != currScreenX) {
                 windowState = 4;
                 clear();
-            }
-            currScreenY = screenY;
-            currScreenX = screenX;
 
-            mvprintw(0, 0, "%s", movementInstruction);
-            mvprintw(1, 0, "%s", turnInstruction);
-            mvprintw(2, 0, "%s", exitInstruction);
+                currScreenY = screenY;
+                currScreenX = screenX;
 
-            for(int x = 0; x < screenX - 1; x++) {
-                mvprintw(ribbonHeight - 1, x, "-");
+                mvprintw(0, 0, "%s", movementInstruction);
+                mvprintw(1, 0, "%s", turnInstruction);
+                mvprintw(2, 0, "%s", exitInstruction);
+
+                for(int x = 0; x < screenX - 1; x++) {
+                    mvprintw(ribbonHeight - 1, x, "-");
+                }
             }
 
             int gridTopRow = ((screenY - ribbonHeight) - tableHeight)/2 + ribbonHeight;
@@ -98,23 +99,22 @@ int main() {
                     mvprintw(gridTopRow + y, gridLeastColumn + x, "%c", printChar);
                 }
             }
+        }
 
-            if(keyInput == KEY_DOWN && selectorY < 3) {
-                selectorY = selectorY + 1;
-            }
+        if(keyInput == KEY_DOWN && selectorY < 3) {
+            selectorY = selectorY + 1;
+        }
 
-            if(keyInput == KEY_UP && selectorY > 1) {
-                selectorY = selectorY - 1;
-            }
+        if(keyInput == KEY_UP && selectorY > 1) {
+            selectorY = selectorY - 1;
+        }
 
-            if(keyInput == KEY_RIGHT && selectorX < 3) {
-                selectorX = selectorX + 1;
-            }
+        if(keyInput == KEY_RIGHT && selectorX < 3) {
+            selectorX = selectorX + 1;
+        }
 
-            if(keyInput == KEY_LEFT && selectorX > 1) {
-                selectorX = selectorX - 1;
-            }
-
+        if(keyInput == KEY_LEFT && selectorX > 1) {
+            selectorX = selectorX - 1;
         }
 
         if(keyInput == '`' || keyInput == '~') {
